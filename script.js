135ffff62d520ebf7b0fb886a3866b4436953ee7
tarcizio502 <tarciziogoncalves502@gmail.com>
2025-09-24 12:42:56 -0300

    Projeto responsivo finalizado.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const meuButao = document.querySelector("button");
-meuButao.addEventListener("click", function () {
-  alert("botão clicado");
-});
+const input = document.querySelector("#meu-input");
+const infos = document.querySelector("h4");
+function cliqueiNoBotao() {
+  infos.innerHTML = input.value;
+  infos.style.color = "blue";
+}
+
+const gostei = document.querySelector("#gostei");
+const naoGostei = document.querySelector("#nao-gostei");
+const h3 = document.querySelector("h5");
+
+let contador = 0; // começa em 0
+
+function clicGostei() {
+  contador++; // soma 1
+  h3.textContent = contador;
+}
+
+function clicNaoGostei() {
+  contador--; // tira 1
+  h3.textContent = contador;
+}
+
+gostei.addEventListener("click", clicGostei);
+naoGostei.addEventListener("click", clicNaoGostei);
